d9ba7b4ccb8333b791ddfb6cb122b3f60700f408
regola-Tacens <regolas_tacens@outlook.fr>
2021-06-13 05:42:18 +0200

    ajout de flex-shrink

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const firstBlock = document.getElementById('box1')
-const secondBlock = document.getElementById('box2')
-const thirdBlock = document.getElementById('box3')
+const box1 = document.getElementById('box1')
+const box2 = document.getElementById('box2')
+const box3 = document.getElementById('box3')
 
 const box1__input = document.getElementsByClassName('box1__input')
 const box2__input = document.getElementsByClassName('box2__input')
@@ -10,19 +10,26 @@ const box1__flexbasis = document.getElementsByClassName('box1__flexbasis')
 const box2__flexbasis = document.getElementsByClassName('box2__flexbasis')
 const box3__flexbasis = document.getElementsByClassName('box3__flexbasis')
 
+const box1__flexshrink = document.getElementsByClassName('box1__flexshrink')
+const box2__flexshrink = document.getElementsByClassName('box2__flexshrink')
+const box3__flexshrink = document.getElementsByClassName('box3__flexshrink')
+
 const container = document.getElementById('container')
 const form__form1 = document.getElementById('form1')
 
 
 getInputValue = ()=> {
-    firstBlock.style.flexGrow= box1__input[0].value
-    firstBlock.style.flexBasis = box1__flexbasis[0].value +'px'
+    box1.style.flexGrow= box1__input[0].value
+    box1.style.flexShrink= box1__flexshrink[0].value
+    box1.style.flexBasis = box1__flexbasis[0].value +'px'
 
-    secondBlock.style.flexGrow= box2__input[0].value
-    secondBlock.style.flexBasis = box2__flexbasis[0].value +'px'
+    box2.style.flexGrow= box2__input[0].value
+    box2.style.flexShrink= box2__flexshrink[0].value
+    box2.style.flexBasis = box2__flexbasis[0].value +'px'
 
-    thirdBlock.style.flexGrow= box3__input[0].value
-    thirdBlock.style.flexBasis= box3__flexbasis[0].value +'px'
+    box3.style.flexGrow= box3__input[0].value
+    box3.style.flexShrink= box3__flexshrink[0].value
+    box3.style.flexBasis= box3__flexbasis[0].value +'px'
 
     reportwidth()
 }
@@ -34,36 +41,36 @@ changeDisplay =()=> {
 }
 
 function reportwidth() {
-    
-    // box1_flexGrow = box1__input[0].value
-    firstBlock.innerHTML = `<div>
+
+
+    box1.innerHTML = `<div>
     <div style="font-size:30px;"><strong>BOX 1</div></strong><br>
-    width : ${firstBlock.clientWidth} px <br><br>
+    width : ${box1.clientWidth} px <br><br>
     flex-grow : ${box1__input[0].value || 1 }<br>
-    flex-shrink : ${ 1 }<br>
+    flex-shrink : ${box1__flexshrink[0].value || 1 }<br>
     flex-basis : ${box1__flexbasis[0].value || 0}px<br><br>
     <b>shorthand</b><br>
-    flex : ${box1__input[0].value || 1 } 1 ${box1__flexbasis[0].value || 0}px
+    flex : ${box1__input[0].value || 1 } ${box3__flexshrink[0].value || 1 } ${box1__flexbasis[0].value || 0}px
     </div>` ;
 
-    secondBlock.innerHTML = `<div>
+    box2.innerHTML = `<div>
     <div style="font-size:30px;"><strong>BOX 2</div></strong><br>
-    width : ${secondBlock.clientWidth} px <br><br>
+    width : ${box2.clientWidth} px <br><br>
     flex-grow : ${box2__input[0].value || 1 }<br>
-    flex-shrink : ${ 1 }<br>
+    flex-shrink : ${box2__flexshrink[0].value || 1 }<br>
     flex-basis : ${box2__flexbasis[0].value || 0} px<br><br>
     <b>shorthand</b><br>
-    flex : ${box2__input[0].value || 1 } 1 ${box2__flexbasis[0].value || 0}px
+    flex : ${box2__input[0].value || 1 } ${box3__flexshrink[0].value || 1 } ${box2__flexbasis[0].value || 0}px
     </div>` ;
 
-    thirdBlock.innerHTML = `<div>
+    box3.innerHTML = `<div>
     <div style="font-size:30px;"><strong>BOX 3</div></strong><br>
-    width : ${thirdBlock.clientWidth} px <br><br>
+    width : ${box3.clientWidth} px <br><br>
     flex-grow : ${box3__input[0].value || 1 }<br>
-    flex-shrink : ${ 1 }<br>
+    flex-shrink : ${box3__flexshrink[0].value || 1 }<br>
     flex-basis : ${box3__flexbasis[0].value || 0} px<br><br>
     <b>shorthand</b><br>
-    flex : ${box3__input[0].value || 1 } 1 ${box3__flexbasis[0].value || 0}px
+    flex : ${box3__input[0].value || 1 } ${box3__flexshrink[0].value || 1 } ${box3__flexbasis[0].value || 0}px
     </div>` ;
     
 
