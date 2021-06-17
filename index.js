d9327af7413e95d1cdf9d7e01702daddec03ec43
regola-Tacens <regolas_tacens@outlook.fr>
2021-06-17 10:15:53 +0200

    factorisation

--- index.js
+++ index.js
@@ -1,103 +1,52 @@
-const box1 = document.getElementById('box1')
-const box2 = document.getElementById('box2')
-const box3 = document.getElementById('box3')
-
-const box1__input = document.getElementsByClassName('box1__input')
-const box2__input = document.getElementsByClassName('box2__input')
-const box3__input = document.getElementsByClassName('box3__input')
-
-const box1__flexbasis = document.getElementsByClassName('box1__flexbasis')
-const box2__flexbasis = document.getElementsByClassName('box2__flexbasis')
-const box3__flexbasis = document.getElementsByClassName('box3__flexbasis')
-
-const box1__flexshrink = document.getElementsByClassName('box1__flexshrink')
-const box2__flexshrink = document.getElementsByClassName('box2__flexshrink')
-const box3__flexshrink = document.getElementsByClassName('box3__flexshrink')
-
-const box1__upArrow = document.getElementById('box1__upArrow')
+const box = document.getElementsByClassName('box')
+const box__input = document.getElementsByClassName('box__input')
+const box__flexbasis = document.getElementsByClassName('box__flexbasis')
+const box__flexShrink = document.getElementsByClassName('box__flexshrink')
 
 const container = document.getElementById('container')
 const form__form1 = document.getElementById('form1')
 
- box1.style.flexGrow = "1"
-
 flexGrowIncrease =(box) => {
-    // console.log(parseInt(box1.style.flexGrow) + 1)
     box.style.flexGrow = (parseInt(box.style.flexGrow) +1).toString()
     reportwidth()
-   
 }
 
 flexGrowDecrease =(box) => {
-    // console.log(parseInt(box1.style.flexGrow) + 1)
     box.style.flexGrow = (parseInt(box.style.flexGrow) -1).toString()
     reportwidth()
-   
 }
 
-
-
 getInputValue = ()=> {
-    box1.style.flexGrow= box1__input[0].value
-    box1.style.flexShrink= box1__flexshrink[0].value
-    box1.style.flexBasis = box1__flexbasis[0].value +'px'
-
-    box2.style.flexGrow= box2__input[0].value
-    box2.style.flexShrink= box2__flexshrink[0].value
-    box2.style.flexBasis = box2__flexbasis[0].value +'px'
-
-    box3.style.flexGrow= box3__input[0].value
-    box3.style.flexShrink= box3__flexshrink[0].value
-    box3.style.flexBasis= box3__flexbasis[0].value +'px'
+    for(i=0; i<box.length;i++){
+        box[i].style.flexGrow= box__input[i].value
+        box[i].style.flexShrink= box__flexShrink[i].value
+        box[i].style.flexBasis = box__flexbasis[i].value +'px'
+    }
 
     reportwidth()
 }
 
 form__form1.addEventListener('change', ()=>getInputValue())
 
-// changeDisplay =()=> {
-//     container.style.flexDirection = 'column'
-// }
-
-reloadPage = () => window.location.reload();
-
 function reportwidth() {
-
-
-    box1.innerHTML = `<div>
-    <div style="font-size:30px;"><strong>BOX 1</div></strong><br>
-    width : ${box1.clientWidth} px <br><br>
-    flex-grow : ${box1.style.flexGrow || 1 }<br>
-    flex-shrink : ${box1__flexshrink[0].value || 1 }<br>
-    flex-basis : ${box1__flexbasis[0].value || 0}px<br><br>
-    <b>shorthand</b><br>
-    flex : ${box1.style.flexGrow || 1 } ${box1__flexshrink[0].value || 1 } ${box1__flexbasis[0].value || 0}px
-    </div>` ;
-
-    box2.innerHTML = `<div>
-    <div style="font-size:30px;"><strong>BOX 2</div></strong><br>
-    width : ${box2.clientWidth} px <br><br>
-    flex-grow : ${box2__input[0].value || 1 }<br>
-    flex-shrink : ${box2__flexshrink[0].value || 1 }<br>
-    flex-basis : ${box2__flexbasis[0].value || 0} px<br><br>
-    <b>shorthand</b><br>
-    flex : ${box2__input[0].value || 1 } ${box2__flexshrink[0].value || 1 } ${box2__flexbasis[0].value || 0}px
-    </div>` ;
-
-    box3.innerHTML = `<div>
-    <div style="font-size:30px;"><strong>BOX 3</div></strong><br>
-    width : ${box3.clientWidth} px <br><br>
-    flex-grow : ${box3__input[0].value || 1 }<br>
-    flex-shrink : ${box3__flexshrink[0].value || 1 }<br>
-    flex-basis : ${box3__flexbasis[0].value || 0} px<br><br>
-    <b>shorthand</b><br>
-    flex : ${box3__input[0].value || 1 } ${box3__flexshrink[0].value || 1 } ${box3__flexbasis[0].value || 0}px
-    </div>` ;
     
-
+    for(i=0; i<box.length;i++){
+        console.log(box[i])
+        box[i].innerHTML =`<div>
+        <div style="font-size:30px;"><strong>BOX ${i +1}</div></strong><br>
+        width : ${box[`${i}`].clientWidth} px<br><br>
+        flex-grow : ${box[`${i}`].style.flexGrow || 1 }<br>
+        flex-shrink : ${box[`${i}`].style.flexShrink || 1 }<br>
+        flex-basis : ${box[`${i}`].style.flexBasis || 0}<br><br>
+        <b>shorthand</b><br>
+        flex : ${box[`${i}`].style.flexGrow || 1 } 
+        ${box[`${i}`].style.flexShrink || 1 } 
+        ${box[`${i}`].style.flexBasis || 0}px
+        </div>`
+    }
   }
 
-
+reloadPage = () => window.location.reload();
 window.onresize = reportwidth;
 window.onload = ()=> {
     reportwidth();
